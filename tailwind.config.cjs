3705bc8cec3ce33bb7570d81aaee7e4c55612191
duraznodev <romanrizodev@gmail.com>
2023-01-26 18:16:36 -0600

    tailwindcss template literals

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  //Safe List Used as Solution for Changing Colors with Template Literals
+  safelist: [
+    { pattern: /bg-(slate|orange)-(100|900)/ },
+    { pattern: /border-(slate|orange)-(100|900)/ },
+    { pattern: /text-(slate|orange)-(100|900)/ },
+  ],
   theme: {
     extend: {},
   },
